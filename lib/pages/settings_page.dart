74e42844c74dcd14fe7063a6829db53d94f91aa3
David Halpin-Roberts <davidhalpinroberts@gmail.com>
2024-07-04 23:13:42 +0100

    Resolved conflict

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -9,15 +9,4 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(title: Text("Settings Page"),)
     );
   }
-}import 'package:flutter/material.dart';
-
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Settings Page"),),
-    );
-  }
 }
