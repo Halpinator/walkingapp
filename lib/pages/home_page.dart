2aea3c9fefb714d6fe5d72a57365f99cd4599a12
David Halpin-Roberts <davidhalpinroberts@gmail.com>
2024-07-04 23:10:00 +0100

    Added settings page and app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:walkingapp/auth/auth_service.dart';
+import 'package:walkingapp/components/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  void logout() {
-    // get auth service
-    final _auth = AuthService();
-    _auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
-        actions: [
-          //logout
-          IconButton(onPressed: logout, icon: Icon(Icons.logout))
-        ],
       ),
+      drawer: MyDrawer(),
     );
   }
 }
